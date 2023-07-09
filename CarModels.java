763563fbcc7d3fb90fbf1b45cab62a372f02ba4b
Ishu9581 <130961999+Ishu9581@users.noreply.github.com>
2023-07-09 21:37:02 +0530

    Create CarModels.java
    Car Dealer Problem

---
+++ CarModels.java
@@ -0,0 +1,13 @@
+package carss;
+
+public class CarModels {
+	public void model() {
+		String[] model= {"Polo Trendline","Polo Highline ","Virtus Trendline ","Virtus Highline","Taigun Trendline ","Taigun Highline","Taigun Topline"};
+		System.out.println("Car Models");
+		for(int i=0;i<model.length;i++) {
+			System.out.println(model[i]);
+		}
+	}
+
+
+}
